b012f8f7273c167bae489aa976599568d47a933b
Sigmabond01 <smdnoor4966@gmail.com>
2025-06-12 05:29:34 +0530

    added analytics

--- Kiban/src/App.jsx
+++ Kiban/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <Router>
         <AppRoutes />
+        <Analytics/>
       </Router>
     </div>
   );
